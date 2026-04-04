14710958d5e9f4522827ba4d7b4d99504eefa5d5
stanlqy9 <sn859627@gmail.com>
2026-04-03 19:25:19 -0700

    made some changes to insertion and selection sort

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -6,22 +6,23 @@ def selection_sort(input_list):
     swaps = 0 # count shifts/assignemnts
 
     n = len(arr)
-    #move through each position in the list 
+    #move through each position in the list
     for i in range(n):
         min_index = i # assume current position has smallest value
-    
-    #look at the smallest value in the rest of the list
-    for j in range (i +1, n):
-        comparisons += 1
-        
-        if arr[j] < arr[min_index]:
-            min_index = j 
-        
-        #swap only if we found the smallest value
-    if min_index != i:
-        arr[i], arr[min_index] = arr[min_index], arr[i]
 
-#return sorted list & counts
+        #look at the smallest value in the rest of the list
+        for j in range(i + 1, n):
+            comparisons += 1
+
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        #swap only if we found the smallest value
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            swaps += 1
+
+    #return sorted list & counts
     return arr, comparisons, swaps
     
 
